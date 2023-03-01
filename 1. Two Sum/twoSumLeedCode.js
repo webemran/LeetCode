e1289bddc5d9ebd3f8fa677e83b077d6fff60840
webemran <webemranbubt@gmail.com>
2023-03-01 23:16:46 +0600

    LeetCode Approved Code

---
+++ 1. Two Sum/twoSumLeedCode.js
@@ -0,0 +1,13 @@
+var twoSum=function(nums, target) {
+    for(let i=0; i<=nums.length;i++){
+        for(let j=1;j<nums.length;j++){
+            if(i==j && nums[i]==nums[j]){
+                j=+j;
+            }
+            else if(nums[i]+nums[j]==target){
+                number=[i,j];
+                return number;
+            }
+        }
+    }
+};
